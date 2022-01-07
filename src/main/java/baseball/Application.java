23732f0c13bc84103413b3a786c49dd03c1c5761
kimtaejun97 <kim_taejun@naver.com>
2022-01-07 16:07:01 +0900

    feat: 컴퓨터의 숫자값 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+
+        // 컴퓨터 숫자 받기.
+        String computerNumber = makeComputerNumber();
+
     }
+
+    private static String makeComputerNumber() {
+        StringBuilder computerNumberBuilder = new StringBuilder();
+
+        while (!isThreeDigit(computerNumberBuilder.toString())) {
+            String number = String.valueOf(Randoms.pickNumberInRange(1, 9));
+
+            if (!isExistNumber(computerNumberBuilder.toString(), number)) {
+                computerNumberBuilder.append(number);
+            }
+        }
+
+        return computerNumberBuilder.toString();
+    }
+
+    private static boolean isThreeDigit(String currComputerNumber) {
+        return currComputerNumber.length() == 3;
+    }
+
+    private static boolean isExistNumber(String currComputerNumber, String number) {
+        return currComputerNumber.indexOf(number) >= 0;
+    }
+
 }
