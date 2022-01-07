dc259f1fb952418da17e018daa0e0b5647b7b352
kimtaejun97 <kim_taejun@naver.com>
2022-01-07 16:08:52 +0900

    feat: 사용자의 입력값 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,6 +13,13 @@ public class Application {
         // 컴퓨터 숫자 받기.
         String computerNumber = makeComputerNumber();
 
+        while (true) {
+            // 입력
+            String userNumber = inputUserNumber();
+
+
+        }
+
     }
 
     private static String makeComputerNumber() {
@@ -35,6 +42,64 @@ public class Application {
 
     private static boolean isExistNumber(String currComputerNumber, String number) {
         return currComputerNumber.indexOf(number) >= 0;
+    }
+
+    private static String inputUserNumber() {
+        String userInput;
+
+        while (true) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            userInput = Console.readLine();
+
+            if (isValidate(userInput)) {
+                break;
+            }
+        }
+
+        return userInput;
+    }
+
+    private static boolean isValidate(String userInput) {
+
+        if (!isValidateLength(userInput)) {
+            System.out.println("[Error] 3자리 숫자를 입력해 주세요.");
+
+            return false;
+        }
+        if (!isValidateNumberRange(userInput)) {
+            System.out.println("[Error] 0이 아닌 숫자만 입력해 주세요.");
+            return false;
+        }
+        if (!isUniqueDigit(userInput)) {
+            System.out.println("[Error] 각 자릿수가 중복되지 않게 입력해 주세요.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isValidateLength(String userInput) {
+        return userInput.length() == 3;
+    }
+
+    private static boolean isValidateNumberRange(String userInput) {
+        for (char number : userInput.toCharArray()) {
+            if (number == '0') {
+                return false;
+            }
+            if (!Character.isDigit(number)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isUniqueDigit(String userInput) {
+        Set<String> uniqueSet = new HashSet<>();
+        uniqueSet.addAll(Arrays.asList(userInput.split("")));
+
+        return uniqueSet.size() == 3;
     }
 
 }
